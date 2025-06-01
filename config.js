4715a2b731a09f467df79f67755e3abf5d6fee53
dumasjoegaens <dumasjoegaens80@gmail.com>
2025-05-31 23:17:46 -0500

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,8 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "      ",
+SESSION_ID: process.env.SESSION_ID || "  https://github.com/dumasjoegaens/GOTAR-XMD.git
+    ",
 // add your Session Id 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
 // make true or false status auto seen
